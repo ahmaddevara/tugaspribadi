cea35a4242fdee9a286e460a31fcf65945cd173f
ahmaddevara <devaraahmad@gmail.com>
2017-06-13 12:08:33 +0700

    commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl327/tugaspribadi/model/ResultsRespons.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl327.tugaspribadi.model;
+
+import java.util.List;
+
+/**
+ * Created by root on 14/05/17.
+ */
+
+
+public class ResultsRespons {
+    public String page;
+    public List<Results> results;
+}
